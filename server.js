ef9947d4df18942d7e8fed5dfdad5da5a2fce0cd
mogadalas <simranchoudhary2021@gmail.com>
2025-03-21 10:32:05 -0600

    Assignment #2 - Resubmission (query in response object)

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ function getJSONObjectForMovieRequirement(req) {
     var json = {
         headers: "No headers",
         key: process.env.UNIQUE_KEY,
-        body: "No body"
+        body: "No body",
+        query: req.query
     };
 
     if (req.body != null) {
